0f5e0d1824a4f3dc75d7e9c866cfdcb036d9428e
cadman77 <cadman77@yandex.ru>
2022-01-22 13:44:08 +0300

    Add project files.

---
+++ ITMO-BIM_m1-lab23/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+//Разработать асинхронный метод для вычисления факториала числа. В методе Main выполнить проверку работы метода.
+
+namespace ITMO_BIM_m1_lab23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1,n2;
+            Console.WriteLine("Введите число A ( 7 < N < 10 ) для вычисления факториала:");
+            n1 = Convert.ToInt32(Console.ReadLine());
+            //Fact(n1);
+            FactAsync(n1);
+
+            Console.WriteLine("Введите число B ( 3 < B < 6 ) для вычисления факториала:");
+            n2 = Convert.ToInt32(Console.ReadLine());
+            FactAsync(n2);
+
+            Console.ReadKey();
+        }
+        static void Fact(int n)
+        {
+            int r = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                r *= i;
+                Thread.Sleep(3000);
+            }
+            Console.WriteLine($"Факториал {n} = {r}");
+        }
+        static async void FactAsync(int n)
+        {
+            await Task.Run(()=>Fact(n));
+        }
+    }
+}
